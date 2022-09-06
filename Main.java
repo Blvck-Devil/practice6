0fd84c089b4b943a285ad398c90682d09035d744
Blvck-Devil <112816848+Blvck-Devil@users.noreply.github.com>
2022-09-06 11:57:36 +0000

    ss

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+import java.util.Scanner;
+public class Main { 
+ public static void main(String[] args){ 
+   Scanner my=new Scanner(System.in);
+  int i,num=0,even=0,odd=0;
+
+   for(i=1;i<=5;i++){
+     System.out.println("Enter number:"+i);
+      num=my.nextInt();
+ }
+   if(num%2==0){
+     even++;
+   }
+   else{
+     odd++;
+   }
+   
+     System.out.println("odd numbers:"+odd);
+   System.out.println("even numbers:"+even);
+   
+ 
+ } 
 }
